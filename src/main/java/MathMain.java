0fe156b17f1f0d170d6962966cfa780dfa53dd71
Thiago Lima <thiago.lima@aurea.com>
2018-10-15 22:19:25 -0700

    Add submodule dependency

--- src/main/java/MathMain.java
+++ src/main/java/MathMain.java
@@ -4,8 +4,10 @@ public class MathMain {
         int a = 10;
         int b = 20;
 
-        System.out.println(a * b);
-
+        System.out.println(MathUtil.multiply(a, b));
+        System.out.println(MathUtil.divide(a, b));
+        System.out.println(MathUtil.sum(a, b));
+        System.out.println(MathUtil.subtract(a, b));
     }
 
 
